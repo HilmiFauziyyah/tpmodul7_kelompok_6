493ae3b75111ad8657bb9ef2fb5e6c4e32df8a47
ibrahimmt11 <abitmulyo@gmail.com>
2023-04-02 21:55:29 +0700

    menambahkan class KuliahMahasiswa

--- tpmodul7_kelompok_6/tpmodul7_kelompok_6/Program.cs
+++ tpmodul7_kelompok_6/tpmodul7_kelompok_6/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace tpmodul7_kelompok_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KuliahMahasiswa_1302210022 b = new KuliahMahasiswa_1302210022();
+            b.ReadJSON();
+            Console.WriteLine();
+
+        }
+    }
+
+}
