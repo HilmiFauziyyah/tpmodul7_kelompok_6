ee3cf43e6bbc6af0c71d0060e2275fc783d90e80
Marsyarayyy <marsyarh09@gmail.com>
2023-04-02 21:05:25 +0700

    menambahkan class <KuliahMahasiswa>

---
+++ tpmodul7_kelompok_6/tpmodul7_kelompok_6/KuliahMahasiswa_1302213003.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.Json;
+class KuliahMahasiswa_1302213003
+{
+ 
+    public void ReadJSON()
+    {
+        string JSONfolder = File.ReadAllText("C:\\Users\\marsy\\Downloads\\KPL\\tpmodul7_kelompok_6\\tpmodul7_kelompok_6\\tpmodul7_kelompok_6\\tp7_2_1302213003.json");
+        DaftarMataKuliah matkul = JsonSerializer.Deserialize<DaftarMataKuliah>(JSONfolder);
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Daftar mata kuliah yang diambil");
+
+        int i = 1;
+        Console.WriteLine(" ");
+        foreach (MataKuliah mataKuliah in matkul.courses)
+        {
+            Console.WriteLine("MK " + i + " " + mataKuliah.code + " - " + mataKuliah.name);
+            i++;
+        }
+
+    }
+    public class DaftarMataKuliah
+    {
+        public List<MataKuliah> courses { get; set; }
+
+        public DaftarMataKuliah() { }
+
+        public DaftarMataKuliah(List<MataKuliah> mataKuliah)
+        {
+            this.courses = mataKuliah;
+        }
+    }
+
+    public class MataKuliah
+    {
+        public string code { get; set; }
+
+        public string name { get; set; }
+
+        public MataKuliah() { }
+
+        public MataKuliah(string code, string name)
+        {
+            this.code = code;
+            this.name = name;
+        }
+    }
+}
--- tpmodul7_kelompok_6/tpmodul7_kelompok_6/Program.cs
+++ tpmodul7_kelompok_6/tpmodul7_kelompok_6/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace tpmodul7_kelompok_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KuliahMahasiswa_1302213003 a = new KuliahMahasiswa_1302213003();    
+            a.ReadJSON();
+            Console.WriteLine();
+
+        }
+    }
+
+}
